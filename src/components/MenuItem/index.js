10bd2c13704e7edfd554453652fa97ce882fc2cf
karenchahal <karenjeet.chahal@gmail.com>
2019-03-11 16:14:32 +0000

    addition of the add to basket

--- src/components/MenuItem/index.js
+++ src/components/MenuItem/index.js
@@ -1,12 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 
 const MenuItem = props => {
   return (
     <div>
       <div> {props.name} </div>
-
+      <div>{props.price}</div>
       <div>{props.description}</div>
-      <img src={props.image} />
+      <img src={props.image} alt="pictures of pizzas" />
+      <button onClick={props.addToBasket}>Add Pizza to Basket</button>
     </div>
   );
 };
